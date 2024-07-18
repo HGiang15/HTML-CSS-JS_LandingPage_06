85fbb911c9eb0048e1bfb22b73b1451869911d7e
HGiang15 <gianghoang150503@gmail.com>
2024-07-18 20:28:54 +0900

    add hero

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// cop menu PC to menu Mobile
+const pcNav = document.querySelector("#pc-nav");
+const mobileNav = document.querySelector("#mobile-nav");
+
+// cop from PC nav to Mobile nav
+mobileNav.innerHTML = pcNav.innerHTML;
